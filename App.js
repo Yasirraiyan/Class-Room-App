928fa9c407460978561898301d2981f5c0e53a1f
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-09-08 00:18:49 +0600

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Teacher from "./components/Teacher";
+//import Login from "./components/Login";
+import Signup from "./components/Signup";
+
+function App() {
+  return (
+    <div>
+      <Teacher />
+    </div>
+  );
+}
+
+export default App;
